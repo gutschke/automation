88b64b754ef0878c7690f9fcb9cbee9cf0a5d915
Markus Gutschke <gutschke@users.noreply.github.com>
2019-03-20 00:28:43 -0700

    Added support for soft fading dimmer values

--- enttec.js
+++ enttec.js
@@ -14,14 +14,29 @@ class Enttec {
     this.reader = null;
     this.waitingForOpen = [ ];
     this.oldData = '';
-    this.values = Array(this.minNumberOfSlots).fill(0);
+    this.values = Array.from({length: this.minNumberOfSlots}, () => 0);
+    this.nominalValues = [ ];
+    this.fader = [ ];
+    this.nextTimeOut = Date.now();
     this.refresh = () => {
+      const now = Date.now();
       if (this.refresh !== null) {
-        this.sendDMXPackage(this.values);
         // DMX likes to receive all dimmer values at least once per second.
         // According to the Wikipedia page, that also ensures that several
         // other timing parameters stay within the expected range.
-        setTimeout(this.refresh, 1000);
+        var tm = 1000;
+        // Keep track of how much we still need to fade changing dimmer values.
+        if (this.fader.length > 0) {
+          tm = this.fadeTimeStep;
+          for (const f of this.fader) {
+            this.values[f.id] =
+              Math.clamp(Math.round(f.dest - f.delta*--f.steps), 0, 255);
+          }
+        }
+        this.fader = this.fader.filter(x => x.steps > 0);
+        this.sendDMXPackage(this.values);
+        this.nextTimeOut = now + tm - Math.max(0, now - this.nextTimeOut);
+        this.timer = setTimeout(this.refresh, this.nextTimeOut - now);
       }
     }
     this.refresh();
@@ -169,9 +184,8 @@ class Enttec {
     // an 8N1 encoding.
     const time = data.length*11/250;
     // Send multiple copies of the same package in order to ensure that we
-    // spend at least 5ms before making other changes.
+    // spend at least a couple of milliseconds before making other changes.
     const reps = Math.max(1, Math.round(this.minTimeMs/time));
-    this.log(`${data} * ${reps}`);
     // Add the required framing for the Enttec DMX widget. 0x7E and 0xE7 are
     // markers to help find packages. "6" is the command for sending DMX
     // packages. And we encode the length in little-endian order.
@@ -186,28 +200,36 @@ class Enttec {
    * Uses the Enttec DMX widget to set the brightness of one or more DMX-
    * addressable dimmers.
    * @param {number} id - the DMX id of the first dimmer.
-   * @param {number} [...] values - one or more dimmer values on scale 0 to 255.
+   * @param {number} fadeTime - rate of change in seconds per 100% change.
+   * @param {number} [...] values - one or more dimmer values on scale 0 to 1.
    */
-  async setDimmer(id, ...values) {
-    this.log(`setDimmer(${id}, ${values})`);
+  async setDimmer(id, fadeTime, ...values) {
     if (id <= 0 || id + values.length > 512) {
       throw new Error('Invalid DMX512 identifiers');
     }
-    if (this.values.length < id) {
-      // Sending values for dimmers that we have never previously controlled.
-      // Add any necessary padding.
-      this.values = this.values.concat(
-        Array.from({length: id-this.values.length}, () => 0))
-        .concat(values);
-    } else {
-      // Sending values for dimmers that we have previously controlled. Update
-      // the cached data.
-      this.values = this.values.slice(0, id).concat(
-        values).concat(
-        this.values.slice(id + values.length));
+    if (this.values.length < id + values.length) {
+      this.values.concat(Array.from(
+        { length: id + value.length - this.values.length }, () => 0));
     }
-    return this.sendDMXPackage(this.values.slice(0, Math.max(
-      this.minNumberOfSlots, id + values.length)));
+    var i = id;
+    values = values.map(x => Math.clamp(x, 0, 1));
+    for (const v of values) {
+      const dest = Math.round(v*255);
+      const diff = dest - (this.values[i]);
+      const steps = Math.max(1, Math.round(Math.abs(diff)*fadeTime*1000/
+                                           (255*this.fadeTimeStep)));
+      const delta = diff/steps;
+      this.fader = this.fader.filter(x => x.id != i);
+      this.fader.push({ id: i, dest: dest, steps: steps, delta: delta });
+      ++i;
+    }
+    clearTimeout(this.timer);
+    this.timer = setTimeout(this.refresh, this.fadeTimeStep);
+    if (this.nominalValues.length < id + values.length) {
+      this.nominalValues.length = id + values.length
+    }
+    this.nominalValues = this.nominalValues.slice(0, id).concat(
+      values).concat(this.nominalValues.slice(id + values.length));
   }
 
   /**
@@ -215,11 +237,11 @@ class Enttec {
    * The light fixture actually has two DMX ids; the first one dims a string
    * of cold LEDs, and the second one dims a string of warm LEDs. By adjusting
    * the mix, we can shift color temperature while dimming brightness.
-   * @param {number} dimmer - the DMX id of the first (i.e. cold) dimmer.
-   * @param {number} [...] values - the desired brightness on a scale of
-   *                                0 to 1.0.
+   * @param {number} id - DMX id of the first dimmer to adjust.
+   * @param {number} fadeTime - rate of change in seconds per 100% change.
+   * @param {number} [...] values - brightness on scale 0 to 1.0.
    */
-  async setDimToGlow(dimmer, ...values) {
+  async setDimToGlow(id, fadeTime, ...values) {
     // In order to achieve a dim-to-glow effect, we vary the rate of change
     // for both the warm and the cold LEDs. For warm LEDs, we start with a
     // high rate of change close to the lower end, and taper off to a slower
@@ -233,17 +255,28 @@ class Enttec {
     // As the manufacturer doesn't publish sufficiently detailed measurements
     // for either the LEDs or the drivers, parameters have been determined
     // empirically to have a pleasing visual effect.
-    const warmCold = [ dimmer ];
+    const warmCold = [ id, fadeTime ];
+    values = values.map(x => Math.clamp(x, 0, 1));
     for (const value of values) {
       const quadratic = (v, initialRate, finalRate, weight) =>
-        Math.clamp(Math.round(
           ((finalRate-initialRate)/(initialRate+finalRate)*v*v +
-           2*initialRate/(initialRate+finalRate)*v)*weight*255), 0, 255);
+           2*initialRate/(initialRate+finalRate)*v)*weight;
       const v = Math.pow(value, 1.2);
       warmCold.push(/* cold LEDs */quadratic(v, 0.5, 1.3, 1));
       warmCold.push(/* warm LEDs */quadratic(v, 1.3, 0.5, .6));
     }
-    return this.setDimmer.apply(this, warmCold);
+    const promise = this.setDimmer.apply(this, warmCold);
+    this.nominalValues = this.nominalValues.slice(0, id).concat(
+      values).concat(this.nominalValues.slice(id + values.length));
+    return promise;
+  }
+
+  /**
+   * Returns the brightness of the given dimmer.
+   * @returns {number} brightness in the range of 0 to 1.
+   */
+  getDimmerValue(id) {
+    return this.nominalValues[id] || 0;
   }
 }
 // Minimum number of DMX slots needed to guarantee that we never send faster
@@ -252,7 +285,9 @@ class Enttec {
 Enttec.prototype.minNumberOfSlots = 24;
 // Restrict updates to at most one every 5ms. Our dimmers sometimes drop
 // DMX packets if we send faster than that.
-Enttec.prototype.minTimeMs = 5;
+Enttec.prototype.minTimeMs = 3;
+// How frequently to refresh dimmers during active fading (in milliseconds).
+Enttec.prototype.fadeTimeStep = 50;
 
 const Logger = {
   /**
@@ -261,7 +296,7 @@ const Logger = {
    * @param {string} msg - The message to be output.
    */
   log(msg) {
-    console.log(msg.replace(/\r/g, '\\r').replace(/\n/g, '\\n'));
+    console.log(String(msg).replace(/\r/g, '\\r').replace(/\n/g, '\\n'));
   }
 };
 Object.assign(Enttec.prototype, Logger);
